d2d0056f6d7f11d100b65cd92357be3c1f42105c
ZhangBa <zhangba@hahaha.com>
2019-08-19 21:24:38 +0800

    数据库  css更新

--- taoku.sql
+++ taoku.sql
@@ -16,3 +16,17 @@ CREATE TABLE tk_user(
 INSERT INTO tk_user VALUES(
 	'10001','one','1760063136@123.com','张大大','15103862070','null',1,1760063166,15103862070
 );
+
+CREATE TABLE tk_index_carousel(
+	cid INT PRIMARY KEY AUTO_INCREMENT,
+	img VARCHAR(64),
+	title VARCHAR(32),
+	href VARCHAR(64)
+);
+INSERT INTO tk_index_carousel VALUES
+	('1','images/index/web-101-101-2.jpg','免费开网店，上淘库商城！','javascript:;'),
+	('2','images/index/web-101-101-5.jpg','皇家龙船酒业品牌专营店','javascript:;'),
+	('3','images/index/web-101-101-4.jpg','健康鼠智能品牌旗舰店','javascript:;'),
+	('4','images/index/web-101-101-1.jpg','盛世无线充电品牌工厂店','javascript:;'),
+	('5','images/index/web-101-101-3.jpg','邦辰生活品牌超级店','javascript:;')
+;
