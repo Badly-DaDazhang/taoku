e8df7a79c0f2cd156253da62bd3919fbb35c51d0
牛逼大大佬 <52398550+Badly-DaDazhang@users.noreply.github.com>
2019-08-27 00:50:10 +0800

    Update taoku.sql

--- taoku.sql
+++ taoku.sql
@@ -23,10 +23,93 @@ CREATE TABLE tk_index_carousel(
 	title VARCHAR(32),
 	href VARCHAR(64)
 );
-INSERT INTO tk_index_carousel VALUES
+INSERT INTO tk_index_carousel VALUES(
 	('1','images/index/web-101-101-2.jpg','免费开网店，上淘库商城！','javascript:;'),
 	('2','images/index/web-101-101-5.jpg','皇家龙船酒业品牌专营店','javascript:;'),
 	('3','images/index/web-101-101-4.jpg','健康鼠智能品牌旗舰店','javascript:;'),
 	('4','images/index/web-101-101-1.jpg','盛世无线充电品牌工厂店','javascript:;'),
 	('5','images/index/web-101-101-3.jpg','邦辰生活品牌超级店','javascript:;')
-;
+);
+CREATE TABLE tk_receiver_adderss(
+	aid INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT,                #用户编号
+  receiver VARCHAR(16),       #接收人姓名
+  province VARCHAR(16),       #省
+  city VARCHAR(16),           #市
+  county VARCHAR(16),         #县
+  address VARCHAR(128),       #详细地址
+  cellphone VARCHAR(16),      #手机
+  fixedphone VARCHAR(16),     #固定电话
+  postcode CHAR(6),           #邮编
+  tag VARCHAR(16),            #标签名
+  is_default BOOLEAN          #是否为当前用户的默认收货地址
+
+); 
+/**购物车条目**/
+CREATE TABLE tk_shoppingcart_item(
+  iid INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT,      #用户编号
+  product_id INT,   #商品编号
+  count INT,        #购买数量
+  is_checked BOOLEAN #是否已勾选，确定购买
+);
+/**商品家族镖 */
+CREATE TABLE tk_goods_family(
+  fid INT PRIMARY KEY AUTO_INCREMENT,
+  fname VARCHAR(32)
+);
+/**商品家族镖 */
+CREATE TABLE tk_goods_family_member(
+  mid INT PRIMARY KEY AUTO_INCREMENT,
+  fid INT ,
+  fname VARCHAR(32)
+);
+/**商品详情表**/
+CREATE TABLE tk_goods(
+  lid INT PRIMARY KEY AUTO_INCREMENT,
+  family_id INT,              #所属商品家族编号
+  title VARCHAR(128),         #主标题
+  price DECIMAL(10,2),        #价格
+  sub_price DECIMAL(10,2),    #辅价格
+  promise VARCHAR(64),        #服务承诺
+  spec VARCHAR(64),           #规格/颜色
+  evaluation INT ,						#商品测评数量
+  gname VARCHAR(32),          #商品名称
+  memory VARCHAR(32),         #内存容量
+  resolution VARCHAR(32),     #分辨率
+  video_card VARCHAR(32),     #显卡型号
+  cpu VARCHAR(32),            #处理器
+  video_memory VARCHAR(32),   #显存容量
+  category VARCHAR(32),       #所属分类
+  disk VARCHAR(32),           #硬盘容量及类型
+  details VARCHAR(1024),      #产品详细说明
+
+  shelf_time BIGINT,          #上架时间
+  sold_count INT,             #已售出的数量
+  is_onsale BOOLEAN           #是否促销中
+);
+/**商品图片**/
+CREATE TABLE tk_goods_pic(
+  pid INT PRIMARY KEY AUTO_INCREMENT,
+  laptop_id INT,              #商品编号
+  sm VARCHAR(128),            #小图片路径
+  md VARCHAR(128),            #中图片路径
+  lg VARCHAR(128)             #大图片路径
+);
+CREATE TABLE tk_order(
+  aid INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT,
+  address_id INT,
+  status INT,             #订单状态  1-等待付款  2-等待发货  3-运输中  4-已签收  5-已取消
+  order_time BIGINT,      #下单时间
+  pay_time BIGINT,        #付款时间
+  deliver_time BIGINT,    #发货时间
+  received_time BIGINT    #签收时间
+)AUTO_INCREMENT=10000000;
+
+CREATE TABLE tk_order_detail(
+  did INT PRIMARY KEY AUTO_INCREMENT,
+  order_id INT,           #订单编号
+  product_id INT,         #产品编号
+  count INT               #购买数量
+);
