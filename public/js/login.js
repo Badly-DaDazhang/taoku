6e54be83c092a43aaab607c516eb4d538fc2c749
Badly <badly.@Badly.local>
2019-09-12 01:34:16 +0800

    补充

--- public/js/login.js
+++ public/js/login.js
@@ -2,9 +2,10 @@ $(function(){
   // 用户选择登陆方式
   $(function(){
     var check = $(".login_top>ul>li>a");
+    // console.log(check[1])
     check.click(function(){
       $(check).addClass("tabulous_active") 
-      .siblings().removeClass("tabulous_active")
+      
     })
   })
 })
